fd974817ba8032aff77c36466cce1e9400d6928e
jesus.carrillo179 <chuchitopimp@hotmail.com>
2020-05-26 16:33:29 -0500

    added get article by id, updated readme

--- database/seeds/001-articles.js
+++ database/seeds/001-articles.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('Articles').truncate()
+  return knex('Articles').del()
     .then(function () {
       // Inserts seed entries
       return knex('Articles').insert([
